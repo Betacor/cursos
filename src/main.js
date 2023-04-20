9b27da761308473daff36fbe037b202da5de4afd
Yurisan Lagos Alfaro <betanae@gmail.com>
2023-04-20 10:27:48 -0400

    init repo

--- src/main.js
+++ src/main.js
@@ -3,4 +3,10 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import BootstrapVue from 'bootstrap-vue/dist/bootstrap-vue.esm';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+Vue.use(BootstrapVue);
+
 createApp(App).use(store).use(router).mount('#app')
