7f89f167f96751deec5b8b4b677a7e224f22c561
Gyrefallon <gonzalo.2800@gmail.com>
2023-04-25 11:24:32 -0400

    Cambia router

--- src/components/admCursos.vue
+++ src/components/admCursos.vue
@@ -12,48 +12,52 @@
       <th scope="col">Estado</th>
       <th scope="col">Imagen</th>
       <th scope="col">Edición</th>
-      <th><button>Agregar</button></th>
+      <th><button class="btn btn-success" @click="this.$router.push('/addCourse')">Agregar</button></th>
     </tr>
   </thead>
   <tbody>
-    <tr v-for="data in items" :key="data.codigo">
-      <th scope="row">{{ data.codigo }}</th>
-      <td>{{data.nombre}}</td>
+    <tr v-for="data in cursos" :key="data.codigo">
+      <th scope="row">{{ data.id }}</th>
+      <td>{{ data.nombre}}</td>
       <td>{{ data.precio }}</td>
       <td>{{ data.duracion }}</td>
       <td>{{ data.descripcion }}</td>
-      <td>{{data.cupos}}</td>
+      <td>{{ data.cupos}}</td>
       <td>{{ data.inscritos }}</td>
       <td>{{ data.estado }}</td>
       <td>{{ data.img }}</td>
-      <!-- @click="editarCurso()" @click="dekCurso()"-->
-      <td><i><button>Editar</button></i><button>Eliminar</button></td>
+      <td><i><button class="btn btn-warning">Editar</button></i><button class="btn btn-danger">Eliminar</button></td>
     </tr>
   </tbody>
 </table>
 </template>
 
 <script>
-import { mapState } from 'vuex';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from "@/services/auth.service";
 
 export default {
-    data(){
-        return{
-            items: []
+  data() {
+    return {
+        nombre:'',
+        cursos: []
+    };
+  },
+  mounted(){
+    this.extraer();
+  },
+  methods: {
+        async extraer() {
+        const querySnapshot = await getDocs(collection(db, "cursos"));
+        querySnapshot.forEach((doc) => {
+            this.cursos.push(doc.data());
+        });
+        },
+        mostrarCurso(){
+            this.cursos.forEach((element)=>{
+            })
         }
-    },
-    computed: {
-		...mapState(['cursos']),
-	},
-    created: async function () {
-		try {
-				let response = this.cursos;
-				this.items = response;
-
-		} catch (error) {
-			this.errorMessage = error;
-		}
-	}
+  }
 }
 </script>
 
